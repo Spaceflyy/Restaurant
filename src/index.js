74c460fe0a9a4cbfdc840b578cf0c599c0caaba7
Spaceflyy <115657060+Spaceflyy@users.noreply.github.com>
2023-04-24 18:32:40 +0100

    Add grid for mobile friendly story section

--- src/index.js
+++ src/index.js
@@ -8,3 +8,11 @@ content.appendChild(createHeader());
 content.appendChild(heroSection());
 content.appendChild(storySection());
 content.appendChild(gallerySection());
+
+window.addEventListener("scroll", () => {
+	if (window.pageYOffset / 600 <= 1) {
+		document.getElementById("header").style.backgroundColor = `rgba(0,0,0,${
+			window.pageYOffset / 600
+		})`;
+	}
+});
